9cb59279c259a787eacb184825e821756e50dd3f
Shrestha, Rupesh <shresthar@ornl.gov>
2018-03-01 08:06:50 -0500

    unzipped MODISTools2.tar.gz

---
+++ MODIS-SOAP-Web-Service/MODISTools2/R/daacmodis.R
@@ -0,0 +1,3 @@
+daac      <- getOption("MODIStools.daac", default = "https://daac.ornl.gov")
+daacmodis <- getOption("MODIStools.daacmodis", default = "https://modis.ornl.gov")
+wsdl_loc  <- getOption("MODIStools.wsdl_loc", default = "/cgi-bin/MODIS/soapservice/MODIS_soapservice.pl")
